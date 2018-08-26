27601179df9c46ff05bf8a083141061de75725b0
Swapnil Bhavsar <swapnil@swapnil.co>
2018-08-26 18:33:27 +0530

    Added assets versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .options({
         processCssUrls: false,
-   });
+   })
+   .version();
